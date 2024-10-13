561d667ea5993b80489cfa20910fbdddce6702d1
SHUBHAM <124447665+s7917@users.noreply.github.com>
2024-10-13 12:34:56 +0530

    resolve clerk

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -62,12 +62,11 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl dark:text-white">
-            Welcome to Squid 🦑
+            Welcome to MockMat Ai 🦑
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500 dark:text-gray-400">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+          MockMate AI provides personalized, real-time interview practice and feedback solutions.
           </p>
         </div>
 
