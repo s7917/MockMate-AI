0024bace19b9ef3a4f672a5046bf8d9f77e777e0
SHUBHAM <124447665+s7917@users.noreply.github.com>
2024-10-13 12:28:27 +0530

    make ui

--- app/page.js
+++ app/page.js
@@ -1,11 +1,134 @@
-import Image from "next/image";
+// "use client"
+// import { Button } from "@/components/ui/button";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card"
+
+// export default function Home() {
+//   return (
+//     <div className="w-full overflow-x-hidden overflow-y-hidden ">
+//        <Card  className='items-center mt-4 h-full justify-center'>
+//   <CardHeader>
+//     <CardTitle>Welcome To MockMate AI</CardTitle>
+//     <CardDescription>Card Description</CardDescription>
+//   </CardHeader>
+//   <CardContent>
+//     <p>Card Content</p>
+//   </CardContent>
+//   <CardFooter>
+//     <p>Card Footer</p>
+//   </CardFooter>
+//   <Button>Click me</Button>
+// </Card>
+
+//     </div>
+//   );
+// }
+
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Header from "./dashboard/_components/Header";
+import { AtomIcon, Edit, Share2 } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
-      <h2>hii i ma leetcode</h2>
-    <Button>Click me</Button>
+      <Image src={'/grid.svg'} className="absolute z-[-10] w-full" 
+      width={1200} height={300} />
+      <Header/>
+     <section className=" z-50">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">Created by</span> <span className="text-sm font-medium">Shubham</span> 
+            <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </a>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to Mockmate-AI</h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Enhance your chances of securing that job offer with our AI-powered interview preparation tool.</p>
+        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="/dashboard" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Click here to start
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="https://www.linkedin.com/in/shubham-b-b35455258" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+<path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+</svg>
+                Linkdin
+            </a>  
+    
+            <a href="https://shubham-personal-portfolio.onrender.com" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                My portfolio
+            </a> 
+
+        </div>
+      
     </div>
+</section>
+<section className="py-3 bg-white z-50 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+<h2 className="font-bold text-3xl">How it Works?</h2>
+<h2 className="text-md text-gray-500">Give mock interview in just 3 simplar easy step</h2>
+
+<div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <a
+        className="block rounded-xl border bg-white
+         border-gray-200 p-8 shadow-xl transition
+         hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+       <AtomIcon className='h-8 w-8'/>
+
+        <h2 className="mt-4 text-xl font-bold text-black">Sign Up  </h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+        AI-Powered Question Generation: The app uses AI to generate interview questions based on the user’s job role, industry, and experience level. By analyzing real-world job descriptions and trends, the AI can create customized and relevant questions that closely mimic actual interview scenarios.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+      <Edit className='h-8 w-8'/>
+
+        <h2 className="mt-4 text-xl font-bold text-black">Edit Your form </h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+        Real-Time Feedback and Analysis: After the user responds to interview questions, the AI analyzes their answers, assessing factors such as tone, clarity, confidence, and content accuracy. It provides real-time feedback with suggestions for improvement in communication and technical responses.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border bg-white border-gray-200 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+      <Share2 className='h-8 w-8' />
+
+        <h2 className="mt-4 text-xl font-bold text-black">Share & Start Accepting Responses</h2>
+
+        <p className="mt-1 text-sm text-gray-600">
+        Personalized Improvement Plan: Based on the user’s performance, the AI generates a tailored interview preparation plan. This plan highlights areas for improvement, suggests additional practice questions, and tracks progress over time, helping users refine their skills effectively before their actual interviews
+        </p>
+      </a>
+
+    
+    </div>
+
+    <div className="mt-12 text-center">
+      <a
+        href="/dashboard"
+        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+      >
+        Get Started Today
+      </a>
+    </div>
+    </section>
+  </div>
+ 
+
   );
 }
